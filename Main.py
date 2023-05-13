f5593051b4908129bdfe044d7574936b1dbe42f0
Asus <ritulaku18@gmail.com>
2023-05-13 12:57:45 +0530

    Add ram in todos

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ user_prompt="Enter a todo:"
 todo1=input(user_prompt)
 todo2=input(user_prompt)
 todo3=input(user_prompt)
-todos= [todo1,todo2,todo3,"hello"]
+todos= [todo1,todo2,todo3,"hello","Ram"]
 print (todos)
 
 
